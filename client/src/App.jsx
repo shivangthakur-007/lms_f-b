29c136385543ccc65398b1cef347544653ac38ab
Dance <thakurshivang579@gmail.com>
2023-10-09 21:02:50 +0530

    commiting some errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
-        <Route path="/course" element={<CourseList />}></Route>
+        <Route path="/courses" element={<CourseList />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/denied" element={<Denied />}></Route>
         <Route
